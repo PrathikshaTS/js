b145a7081744839dd358ebdee09e7a430e28e465
PrathikshaTS <prathikshats6105@gmail.com>
2025-02-27 10:30:55 +0000

    convertion operation doing

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let a="33"
+console.log(typeof a);
+console.log(typeof(a));
+let valInNumber=Number(a);
+console.log(typeof valInNumber);
+console.log(valInNumber);
+
+
+
+let b="ab33"
+console.log(typeof b);
+
+let valInNum=Number(b);
+console.log(typeof valInNum);//number
+console.log(valInNum);//NaN not a number
+
+//"33"=>33
+//"33abc"=>NaN
+//true/false=>1/0
+let c=1
+let booleanVal=Boolean(c);
+console.log(booleanVal);
+
+//""=>flase
+//"Prathik"=>true
