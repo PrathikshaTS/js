930c17257a45c86506e1cbbfcabfd85b29198b91
PrathikshaTS <prathikshats6105@gmail.com>
2025-02-27 10:09:26 +0000

    dataTypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const id=123
+let name="Prathik"
+var number="1234567"
+city="Tumkur"
+
+name="Rohan"
+
+number=90
+// id=345
+console.table([id,name,number,city])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//to say that the code we writ is for new version of js
+// alert(3+3)//not allowed in node js :allowed in browser
+//number=>2 to power 53
+//string=>""
+//boolean=>true/false
+//bigint
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object
+
+let age=19
+let name="Prathik"
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof "HELLO");
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
