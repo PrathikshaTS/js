4e15b0fe1f47431d764f332c145d4d35d6d5fab7
PrathikshaTS <prathikshats6105@gmail.com>
2025-02-28 06:16:59 +0000

    datatypes summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,25 +1,89 @@
-let a="33"
-console.log(typeof a);
-console.log(typeof(a));
-let valInNumber=Number(a);
-console.log(typeof valInNumber);
-console.log(valInNumber);
+// let a="33"
+// console.log(typeof a);
+// console.log(typeof(a));
+// let valInNumber=Number(a);
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
 
 
 
 let b="ab33"
-console.log(typeof b);
+// console.log(typeof b);
 
 let valInNum=Number(b);
-console.log(typeof valInNum);//number
-console.log(valInNum);//NaN not a number
+// console.log(typeof valInNum);//number
+// console.log(valInNum);//NaN not a number
 
 //"33"=>33
 //"33abc"=>NaN
 //true/false=>1/0
 let c=1
 let booleanVal=Boolean(c);
-console.log(booleanVal);
+// console.log(booleanVal);
 
 //""=>flase
 //"Prathik"=>true
+
+
+    //-----------------------operations----------------------------
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="Hello"
+// let str2=" Prathik"
+// let res=str1+str2;
+// console.log(res);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+// console.log("2">1);//true
+// console.log("02">1);//true
+
+// console.log(null>0);//false
+// console.log(null>=0);//true
+// //comparators convert null to number treating it as 0 in above situations
+
+// console.log(null==0);//false
+
+// console.log(undefined>0);//false
+
+ //===
+//strict check =>checks datatypes also
+// console.log("2"==2);//true
+// console.log("2"===2);//false
+
+// const id=Symbol('123');
+// const anotherId=Symbol('123');
+// console.log(id===anotherId);//false
+
+// const bigNum=34534567892345645645n
+// console.log(typeof bigNum);
+
+const heroes=["venom","shaktiman","spiderman","hulk"];
+//object
+let myObj={
+    name:"Prathiksha",
+    age:19,
+}
+
+const myfunc=function() {
+    console.log("hello");
+    
+}
+myfunc
+
+
+
+
+
+
