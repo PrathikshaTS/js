89b3c1d953f48aceadbbe147cf3a261d7daa37a0
PrathikshaTS <prathikshats6105@gmail.com>
2025-03-02 12:28:33 +0000

    arrays part1 done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// const arr=[0,1,34,true,"Prathik"];//valid
+// let arr2=arr;//shallow copy
+
+
+ const heroes=["spiderman","hulk","venom","shaktiman"];
+// console.log(heroes[2]);
+
+// const arr1=new Array(4,5,6,7,10);
+// console.log(arr1);
+// arr1.push(99);//add at end
+// console.log(arr1);
+// console.log(arr1.length);
+// console.log(arr1.pop());//remove from end
+
+// arr1.unshift(98);//add at first
+// console.log(arr1);
+// arr1.shift();//remove from first
+// console.log(arr1);
+
+// console.log(arr1.includes(99));
+// console.log(arr1.indexOf(5));
+
+// const newArr=arr1.join();
+// console.log(newArr);
+// console.log(typeof newArr);//string
+
+//slice and splice
+//------------------------important-----------------
+const arr=[0,1,2,3,4,5];
+console.log("A ",arr);
+let arr1=arr.slice(1,3);
+console.log("B ",arr);
+let arr2=arr.splice(1,3);
+console.log("C ",arr);
+console.log(arr1);
+console.log(arr2);
+
+
+
+
