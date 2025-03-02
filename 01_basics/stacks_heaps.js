c56a4409e5fc9884014dc6b4907039c810bd9b97
PrathikshaTS <prathikshats6105@gmail.com>
2025-03-02 04:37:39 +0000

    stacks and heaps

---
+++ 01_basics/stacks_heaps.js
@@ -0,0 +1,18 @@
+
+// let name="Prathik"
+// let name2=name;//copy of value is given
+// console.log(name);
+// console.log(name2);
+// name2="Dev";
+// console.log(name2);
+
+let user1={
+    name:"Prathik",
+    usn:"1si23cs139"
+}
+let user2=user1;
+console.log(user1);
+console.log(user2);
+user2.usn=567;
+console.log(user1);
+console.log(user2);
