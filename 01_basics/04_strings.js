70503fe0c6f06c9bc6f5781c68c69a27c08f5856
PrathikshaTS <prathikshats6105@gmail.com>
2025-03-02 07:23:36 +0000

    strings done

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,33 @@
+const name="Prathik"
+let repoCount=1;
+//string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// let user=new String("Prathik");
+// console.log(user[6]);
+// console.log(user.__proto__);
+// console.log(user.length);
+// console.log(user.toUpperCase());
+// console.log(user.charAt(5));
+// console.log(user.indexOf('i'));
+
+let user=new String("Prathik-VS");
+// let str=user.slice(0,7);//here excluding 7 th index
+// console.log(str);
+// let str2=user.slice(7);
+// console.log(str2);
+// let str1=user.slice(-3,12);
+// console.log(str1);
+
+
+// let str=user.substring(2,7);
+// console.log(str);
+
+// let str="   Prathik  ";
+// console.log(str);
+// console.log(str.trim());
+
+//inludes and replace method 
+
+let str=new String("Prathik-VS-hello");
+console.log(str.split('-'));
