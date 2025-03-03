5690736790477361c39363deb7192960b46c1f53
PrathikshaTS <prathikshats6105@gmail.com>
2025-03-03 07:31:22 +0000

    part2 done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,42 @@
+//singleton objects is created using constructor
+
+
+const user=new Object();
+user.name="Sammy";
+user.id="abc139";
+user.age=19;
+user.isLoggedIn=false;
+// console.log(user);
+
+//object ke ander objects
+const regularUser={
+    email:"sammy@google.com",
+    userfullname:{
+        fullname:{
+            firstName:"Sammy",
+            lastName:"Man"
+        }
+    }
+}
+// console.log(regularUser);
+// console.log(regularUser.userfullname);
+// console.log(regularUser.userfullname.fullname);
+// console.log(regularUser.userfullname.fullname.firstName);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+// const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2)
+const obj3={...obj1,...obj2}//spread operator
+// console.log(obj3);
+
+//there can be objects in arrays
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+console.log(user.hasOwnProperty('isLogged'));
+
+
+
+
