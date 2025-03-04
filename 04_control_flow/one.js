cad681f95a129b0f3da9359ad9a26f3bee5eebc3
PrathikshaTS <prathikshats6105@gmail.com>
2025-03-04 09:50:40 +0000

    if else done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+// <
+// >
+// <=
+// >=
+// ==
+// !=
+// === also checks type
+
+// if( 2=="2"){//but 2 is not equal to "2"
+//     console.log("hello");
+// }
+
+if (2==="2") {
+    console.log("hello");
+    
+}else{
+    console.log("bye");
+    
+}
+
+// if(3!=4)console.log("hello"),console.log("bye");
+
+// if (x>y) {
+    
+// } else if(1>7){
+    
+// }else{
+
+// }
+
+//-----------------------------------------------------
+// &&,||
